c1d81c405df4b2c86f73095a6e6096ae72fb4f16
tathagata17 <tathagata14n14@gmail.com>
2025-07-11 10:07:25 +0530

    done and dusted

--- src/main/java/App/controller/controller.java
+++ src/main/java/App/controller/controller.java
@@ -66,7 +66,7 @@ public class controller {
 		String userName = jwtservice.extractUsername(token);
 		todo.setEmail(userName);
 		todoService.savetodo(todo);
-		return "done";
+		return "done and dusted";
 	}
 
 	@DeleteMapping("/deletetodo/{id}")
